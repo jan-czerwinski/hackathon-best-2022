0bfcac4ac64e923f57179a7a9f3358c508f21da2
maciek <macieq2048@gmail.com>
2021-12-26 17:17:04 +0100

    basic non max supression

--- src/edge_detection.py
+++ src/edge_detection.py
@@ -27,10 +27,50 @@ class EdgeDetector:
         g = np.hypot(gx, gy)
         # normalization
         g = g / g.max() * 255
-        
+
         teta = np.arctan2(gx, gy)
 
         return g, teta
+
+    def non_max_suppression(self, src_img: np.ndarray, threshold: np.ndarray):
+        src_width, src_height = src_img.shape
+
+        out_img = np.zeros((src_width, src_height), dtype=np.int32)
+        angle = threshold * 180. / np.pi
+        angle[angle < 0] += 180
+
+        for i in range(1, src_width - 1):
+            for j in range(1, src_height - 1):
+                try:
+                    q = 255
+                    r = 255
+
+                    # angle 0
+                    if (0 <= angle[i, j] < 22.5) or (157.5 <= angle[i, j] <= 180):
+                        q = src_img[i, j + 1]
+                        r = src_img[i, j - 1]
+                    # angle 45
+                    elif 22.5 <= angle[i, j] < 67.5:
+                        q = src_img[i + 1, j - 1]
+                        r = src_img[i - 1, j + 1]
+                    # angle 90
+                    elif 67.5 <= angle[i, j] < 112.5:
+                        q = src_img[i + 1, j]
+                        r = src_img[i - 1, j]
+                    # angle 135
+                    elif 112.5 <= angle[i, j] < 157.5:
+                        q = src_img[i - 1, j - 1]
+                        r = src_img[i + 1, j + 1]
+
+                    if (src_img[i, j] >= q) and (src_img[i, j] >= r):
+                        out_img[i, j] = src_img[i, j]
+                    else:
+                        out_img[i, j] = 0
+
+                except IndexError as e:
+                    pass
+
+        return out_img
 
     # img as ndarray with shape (height, width, channels)
     def edge_detection(self, img: np.ndarray) -> np.ndarray:
@@ -44,6 +84,8 @@ class EdgeDetector:
 
         # finding intensity gradient
         filtered_image, teta = self.intensity_gradient(filtered_image)
+
+        filtered_image = self.non_max_suppression(filtered_image, teta)
 
         return filtered_image
 
